29936db54bf33a3b79cb83b5b1936198207fe0dd
wlandau <will.landau@gmail.com>
2020-12-21 15:42:13 -0500

    Add first vignette

--- R/tar_jags_example_data.R
+++ R/tar_jags_example_data.R
@@ -5,16 +5,21 @@
 #' @return List, dataset compatible with the model file from
 #'   [tar_jags_example_file()].
 #' @param n Integer of length 1, number of data points.
+#' @param true_params Logical, whether to include the true values of
+#'   the parameters used to simulate the data.
 #' @examples
 #' tar_jags_example_data()
-tar_jags_example_data <- function(n = 10L) {
+tar_jags_example_data <- function(n = 10L, true_params = TRUE) {
   true_beta <- stats::rnorm(n = 1, mean = 0, sd = 1)
   x <- seq(from = -1, to = 1, length.out = n)
   y <- stats::rnorm(n, x * true_beta, 1)
-  list(
+  out <- list(
     n = n,
     x = x,
-    y = y,
-    true_beta = true_beta
+    y = y
   )
+  if (true_params) {
+    out$true_beta <- true_beta
+  }
+  out
 }
